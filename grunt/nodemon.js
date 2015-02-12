67e02e97713900ec63352eeaddbe86e871ead51b
Rogelio Morrell <molekilla@gmail.com>
2015-02-12 12:22:07 -0500

    updating nodemon task

--- grunt/nodemon.js
+++ grunt/nodemon.js
@@ -1,6 +1,7 @@
 module.exports = {
   dev: {  
     options: {
+      ext: '<%= nodemon.ext || "vb, vbhtml, cs, cshtml, json, css, js, jshtml" %>',
       exec: '<%= nodemon.exec %>',
       delay: '<%= nodemon.delay || 900 %>',
       env: {
